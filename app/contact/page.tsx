a4be80f5af83aaa6d880da4a73f0ca84b825ab22
Mitwen <mitwen18@gmail.com>
2026-02-13 21:44:16 +0530

    Create page.tsx

---
+++ app/contact/page.tsx
@@ -0,0 +1,9 @@
+import ContactForm from '../../components/ContactForm';
+
+export default function ContactPage() {
+  return (
+    <div style={{ paddingTop: '80px' }}>
+      <ContactForm />
+    </div>
+  );
+}
