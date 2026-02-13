890c15bce454eef236f76affcdbc26e3896425c4
Mitwen <mitwen18@gmail.com>
2026-02-13 21:42:41 +0530

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Hero from '../components/Hero';
+import WorksGrid from '../components/WorksGrid';
+import PricingSection from '../components/PricingSection';
+import ContactForm from '../components/ContactForm';
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <WorksGrid />
+      <PricingSection />
+      <ContactForm />
+    </>
+  );
+}
