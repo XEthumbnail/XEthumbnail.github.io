24830591200b009de3b77ec7f70d020f9b49e39d
Mitwen <mitwen18@gmail.com>
2026-02-13 21:43:50 +0530

    Create page.tsx

---
+++ app/pricing/page.tsx
@@ -0,0 +1,9 @@
+import PricingSection from '../../components/PricingSection';
+
+export default function PricingPage() {
+  return (
+    <div style={{ paddingTop: '80px' }}>
+      <PricingSection />
+    </div>
+  );
+}
