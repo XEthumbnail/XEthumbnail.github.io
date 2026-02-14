fe3fb7167e97fdb2527dffcfbba58ec595731556
Mitwen <mitwen18@gmail.com>
2026-02-14 21:49:40 +0530

    Update galleryData.ts

--- data/galleryData.ts
+++ data/galleryData.ts
@@ -7,22 +7,22 @@ export interface GalleryItem {
 export const galleryData: GalleryItem[] = [
   {
     id: 1,
-    imageUrl: '/Freelancing-/images/gallery/one.png',
+    imageUrl: '/images/gallery/one.png',
     alt: 'Tech Thumbnail'
   },
   {
     id: 2,
-    imageUrl: '/Freelancing-/images/gallery/two.png',
+    imageUrl: '/images/gallery/two.png',
     alt: 'Tutorial Thumbnail'
   },
   {
     id: 3,
-    imageUrl: '/Freelancing-/images/gallery/three.png',
+    imageUrl: '/images/gallery/three.png',
     alt: 'Bot Thumbnail'
   },
   {
     id: 4,
-    imageUrl: '/Freelancing-/images/gallery/four.png',
+    imageUrl: '/images/gallery/four.png',
     alt: 'Tutorial Thumbnail'
   }
 ];
