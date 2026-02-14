8a89d40667c0d3ae51071c667561399f5e7071fc
Mitwen <mitwen18@gmail.com>
2026-02-14 09:33:22 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,9 @@
 import Hero from '../components/Hero';
-import WorksGrid from '../components/WorksGrid';
-import PricingSection from '../components/PricingSection';
-import ContactForm from '../components/ContactForm';
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <WorksGrid />
-      <PricingSection />
-      <ContactForm />
     </>
   );
 }
